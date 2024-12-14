54316734be46d7c5537d08c053e07ffcd778927f
Kroszborg <abhimanpanwar6@gmail.com>
2024-12-15 01:22:18 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Disable telemetry
-  telemetry: {
-    disabled: true
-  },
-  // Ignore build errors
-  typescript: {
-    ignoreBuildErrors: true
-  },
-  eslint: {
-    ignoreDuringBuilds: true
-  }
 };
 
-module.exports = nextConfig;
+export default nextConfig;
